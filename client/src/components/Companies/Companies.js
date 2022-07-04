ed81c879881226faaee5e394568924e5f7c7a933
TMysiv <tarasmysiv@gmail.com>
2022-07-04 23:39:52 +0300

    add client logout,update,new company form

--- client/src/components/Companies/Companies.js
+++ client/src/components/Companies/Companies.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import jwt_decode from "jwt-decode";
+import {companyService} from "../../services/company.service";
+
+import css from './style.css'
 
 const Companies = () => {
+
+    const [companies, setCompanies] = useState([]);
+
+    const token = localStorage.getItem('token');
+    const {userId} = jwt_decode(token);
+
+    useEffect(() => {
+        companyService.getAll(userId).then(value => setCompanies(value))
+    }, [])
+
+    const renderTableData = () => {
+        return companies.map((company) => {
+            const {id, name, address, numberOfEmployees, type} = company;
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{address}</td>
+
+                    <td>{numberOfEmployees}</td>
+
+                    <td>{type}</td>
+                    <button>update</button>
+                    <button>delete</button>
+                </tr>
+            )
+        })
+    }
+
+    const renderTableHeader = () => {
+        const header = ['id', 'name', 'address', 'numberOfEmployees', 'type', 'buttons'];
+        return header.map(key => {
+            return <th key={new Date().getDate()}>{key.toUpperCase()}</th>
+        })
+    }
+
+
+
     return (
-        <div>
-            Companies
+        <div className={'table'}>
+
+            <h2 id='title'>Your companies</h2>
+            <table id='companies'>
+                <tbody>
+                <tr>{renderTableHeader()}</tr>
+                {renderTableData()}
+                </tbody>
+            </table>
+
+
         </div>
     );
 };
--- client/src/services/company.service.js
+++ client/src/services/company.service.js
@@ -1,6 +1,6 @@
 import {apiWithInterceptor} from "./axios.service";
 
 export const companyService = {
-    // getAll: () =>,
-    createCompany: (id,data) =>apiWithInterceptor.post(`companies/${id}`,data)
+    getAll: (id) => apiWithInterceptor.get(`companies/${id}/all`).then(value => value.data) ,
+    createCompany: (id,data) => apiWithInterceptor.post(`companies/${id}`,data).then(value => value.data)
 }
